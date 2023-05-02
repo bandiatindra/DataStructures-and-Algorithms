d4e87845dfe440e67da9b57b32bce75b386a87a1
Atindra Bandi <bandiatindra@gmail.com>
2023-05-02 02:18:29 -0700

    Update LC 1911. Max Alternating Subsequence.py

--- Additional Algorithms/LC 1911. Max Alternating Subsequence.py
+++ Additional Algorithms/LC 1911. Max Alternating Subsequence.py
@@ -1,3 +1,4 @@
+# The idea here is to calculate the max before current index . The idea issimilar to Kadane's Algorithm
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         prevmaxodd = nums[0]
